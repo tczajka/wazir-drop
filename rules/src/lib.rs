7d5609d0ea5fd0c1fdf837b14783fbe8fd85929f
Tomek Czajka <tczajka@gmail.com>
2025-09-14 02:46:58 -0700

    Position, Move structs

--- rules/src/lib.rs
+++ rules/src/lib.rs
@@ -1,9 +1,11 @@
 mod bitboard;
 mod consts;
 mod error;
+mod position;
 mod square;
 
 pub use bitboard::Bitboard;
 pub use consts::{BOARD_HEIGHT, BOARD_SIZE, BOARD_WIDTH};
 pub use error::ParseError;
+pub use position::{Color, Location, Move, PieceType, Position};
 pub use square::Square;
