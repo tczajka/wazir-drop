038c77c8ef9189b8624150037e5a7f019e46d823
Tomek Czajka <tczajka@gmail.com>
2025-10-17 19:01:57 -0700

    Draw captured spaces.

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -2,11 +2,11 @@ use eframe::{
     App,
     egui::{
         self, Align2, CentralPanel, Color32, FontId, Image, Pos2, Rect, Sense, SidePanel, Theme,
-        Vec2, ViewportBuilder, include_image,
+        Ui, Vec2, ViewportBuilder, include_image,
     },
 };
 use wazir_drop::{
-    Color, ColoredPiece, Coord, Square,
+    Color, ColoredPiece, Coord, Piece, PieceNonWazir, Square,
     enums::{EnumMap, SimpleEnumExt},
 };
 
@@ -25,6 +25,7 @@ fn main() {
 struct WazirDropApp {
     reverse: bool,
     piece_images: EnumMap<ColoredPiece, Image<'static>>,
+    tile_size: f32,
 }
 
 impl App for WazirDropApp {
@@ -47,6 +48,7 @@ impl WazirDropApp {
         Self {
             reverse: false,
             piece_images: Self::piece_images(),
+            tile_size: 0.0,
         }
     }
 
@@ -77,12 +79,47 @@ impl WazirDropApp {
         }
     }
 
-    fn update_chessboard(&mut self, ui: &mut egui::Ui) {
-        let (response, painter) = ui.allocate_painter(ui.available_size(), Sense::click());
-        let size = response.rect.size();
-        let tile_size =
-            (size.x / (Coord::WIDTH + 2) as f32).min(size.y / (Coord::HEIGHT + 2) as f32);
+    fn update_chessboard(&mut self, ui: &mut Ui) {
+        let size = ui.available_size();
+        self.tile_size = (size.x / (Coord::WIDTH + PieceNonWazir::COUNT + 3) as f32)
+            .min(size.y / (Coord::HEIGHT + 2) as f32);
 
+        self.draw_coordinates(ui);
+        self.update_squares(ui);
+        self.update_captured(ui);
+
+        // test
+        self.draw_piece(ui, Square::E1, ColoredPiece::RedWazir);
+    }
+
+    fn square_rect(&self, square: Square) -> Rect {
+        let coord = Coord::from(square.pov(self.pov_color()));
+        Rect::from_min_size(
+            Pos2::new(
+                (coord.x() + 1) as f32 * self.tile_size,
+                (coord.y() + 1) as f32 * self.tile_size,
+            ),
+            Vec2::new(self.tile_size, self.tile_size),
+        )
+    }
+
+    fn captured_rect(&self, color: Color, piece: PieceNonWazir) -> Rect {
+        let x = Coord::WIDTH + 2 + piece.index();
+        let y = 1
+            + (Coord::HEIGHT - 1)
+                * (if self.reverse {
+                    color.opposite().index()
+                } else {
+                    color.index()
+                });
+
+        Rect::from_min_size(
+            Pos2::new(x as f32 * self.tile_size, y as f32 * self.tile_size),
+            Vec2::new(self.tile_size, self.tile_size),
+        )
+    }
+
+    fn draw_coordinates(&self, ui: &mut Ui) {
         for y in 0..Coord::HEIGHT {
             let y_on_screen = if self.reverse {
                 Coord::HEIGHT - y - 1
@@ -90,11 +127,14 @@ impl WazirDropApp {
                 y
             };
             let name = char::from(b'a' + y as u8);
-            painter.text(
-                Pos2::new(0.7 * tile_size, (y_on_screen as f32 + 1.5) * tile_size),
+            ui.painter().text(
+                Pos2::new(
+                    0.7 * self.tile_size,
+                    (y_on_screen as f32 + 1.5) * self.tile_size,
+                ),
                 Align2::CENTER_CENTER,
                 name,
-                FontId::monospace(0.3 * tile_size),
+                FontId::monospace(0.3 * self.tile_size),
                 Color32::BLACK,
             );
         }
@@ -106,48 +146,48 @@ impl WazirDropApp {
                 x
             };
             let name = char::from(b'1' + x as u8);
-            painter.text(
-                Pos2::new((x_on_screen as f32 + 1.5) * tile_size, 0.7 * tile_size),
+            ui.painter().text(
+                Pos2::new(
+                    (x_on_screen as f32 + 1.5) * self.tile_size,
+                    0.7 * self.tile_size,
+                ),
                 Align2::CENTER_CENTER,
                 name,
-                FontId::monospace(0.3 * tile_size),
+                FontId::monospace(0.3 * self.tile_size),
                 Color32::BLACK,
             );
         }
+    }
 
+    fn update_squares(&mut self, ui: &mut Ui) {
         for square in Square::all() {
-            painter.rect_filled(
-                self.square_rect(square, tile_size),
-                0.0,
-                Self::square_color(square),
-            );
+            let rect = self.square_rect(square);
+            if ui.allocate_rect(rect, Sense::click()).clicked() {
+                self.click_square(square);
+            }
+            ui.painter()
+                .rect_filled(rect, 0.0, Self::square_color(square));
         }
+    }
 
-        self.piece_images[ColoredPiece::RedWazir]
-            .paint_at(ui, self.square_rect(Square::E1, tile_size));
-
-        if response.clicked()
-            && let Some(click) = response.interact_pointer_pos()
-        {
-            let x = (click.x / tile_size - 1.0).floor();
-            let y = (click.y / tile_size - 1.0).floor();
-            if x >= 0.0 && x < Coord::WIDTH as f32 && y >= 0.0 && y < Coord::HEIGHT as f32 {
-                let square =
-                    Square::from_coord(Coord::new(x as usize, y as usize)).pov(self.pov_color());
-                self.click(square);
+    fn update_captured(&mut self, ui: &mut Ui) {
+        for color in Color::all() {
+            for piece in PieceNonWazir::all() {
+                let rect = self.captured_rect(color, piece);
+                if ui.allocate_rect(rect, Sense::click()).clicked() {
+                    self.click_captured(color, piece);
+                }
+                ui.painter().rect_filled(
+                    rect,
+                    0.0,
+                    Self::square_color(Square::from_index(piece.index())),
+                );
             }
         }
     }
 
-    fn square_rect(&self, square: Square, tile_size: f32) -> Rect {
-        let coord = Coord::from(square.pov(self.pov_color()));
-        Rect::from_min_size(
-            Pos2::new(
-                (coord.x() + 1) as f32 * tile_size,
-                (coord.y() + 1) as f32 * tile_size,
-            ),
-            Vec2::new(tile_size, tile_size),
-        )
+    fn draw_piece(&self, ui: &mut Ui, square: Square, piece: ColoredPiece) {
+        self.piece_images[piece].paint_at(ui, self.square_rect(square));
     }
 
     fn pov_color(&self) -> Color {
@@ -158,7 +198,14 @@ impl WazirDropApp {
         }
     }
 
-    fn click(&mut self, square: Square) {
+    fn click_square(&mut self, square: Square) {
         println!("Clicked on square {square}");
+    }
+
+    fn click_captured(&mut self, color: Color, piece: PieceNonWazir) {
+        println!(
+            "Clicked on captured {piece}",
+            piece = Piece::from(piece).with_color(color)
+        );
     }
 }
