005a5a6293e3d89c74d72819f47eefd8926cd6cd
Tomek Czajka <tczajka@gmail.com>
2025-10-23 14:40:11 -0700

    Trivial MainPlayer

---
+++ src/main_player.rs
@@ -0,0 +1,32 @@
+use crate::{clock::Timer, movegen, Color, Move, Player, PlayerFactory, Position};
+use std::time::Duration;
+
+#[derive(Debug)]
+pub struct MainPlayer;
+
+impl MainPlayer {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Player for MainPlayer {
+    fn make_move(&mut self, position: &Position, _timer: &Timer) -> Move {
+        movegen::pseudomoves(position).next().expect("Stalemate")
+    }
+}
+
+#[derive(Debug)]
+pub struct MainPlayerFactory;
+
+impl PlayerFactory for MainPlayerFactory {
+    fn create(
+        &self,
+        _color: Color,
+        _opening: &[Move],
+        _time_limit: Option<Duration>,
+    ) -> Box<dyn crate::Player> {
+        Box::new(MainPlayer::new())
+    }
+}
