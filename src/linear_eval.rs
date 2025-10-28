6eaf5950aef3bc68d7fff8184d7715254b729af1
Tomek Czajka <tczajka@gmail.com>
2025-10-27 22:38:18 -0700

    Store linear weights in i16

--- src/linear_eval.rs
+++ src/linear_eval.rs
@@ -3,8 +3,8 @@ use crate::{enums::EnumMap, Color, Evaluator, Features};
 #[derive(Debug)]
 pub struct LinearEvaluator<F> {
     features: F,
-    to_move_weight: i32,
-    feature_weights: Vec<i32>,
+    to_move_weight: i16,
+    feature_weights: Vec<i16>,
 }
 
 impl<F: Features> LinearEvaluator<F> {
@@ -12,8 +12,8 @@ impl<F: Features> LinearEvaluator<F> {
         assert_eq!(feature_weights.len(), features.count());
         Self {
             features,
-            to_move_weight: to_move_weight.into(),
-            feature_weights: feature_weights.iter().map(|&w| w.into()).collect(),
+            to_move_weight,
+            feature_weights: feature_weights.to_vec(),
         }
     }
 }
@@ -31,14 +31,14 @@ impl<F: Features> Evaluator for LinearEvaluator<F> {
     }
 
     fn add_feature(&self, accumulator: &mut Self::Accumulator, feature: usize) {
-        *accumulator += self.feature_weights[feature];
+        *accumulator += i32::from(self.feature_weights[feature]);
     }
 
     fn remove_feature(&self, accumulator: &mut Self::Accumulator, feature: usize) {
-        *accumulator -= self.feature_weights[feature];
+        *accumulator -= i32::from(self.feature_weights[feature]);
     }
 
     fn evaluate(&self, accumulators: &EnumMap<Color, Self::Accumulator>, to_move: Color) -> i32 {
-        accumulators[to_move] - accumulators[to_move.opposite()] + self.to_move_weight
+        accumulators[to_move] - accumulators[to_move.opposite()] + i32::from(self.to_move_weight)
     }
 }
