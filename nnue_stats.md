08766aeb68e357e650874ae54a68873902b54258
Tomek Czajka <tczajka@gmail.com>
2025-11-19 21:59:59 +0100

    Plot activations

---
+++ nnue_stats.md
@@ -0,0 +1,27 @@
+# Embedding weights
+
+![weights_0](data/weights_0.svg)
+
+# Embedding activations
+
+![activations_0](data/activations_0.svg)
+
+# Hidden layer 1 weights
+
+![weights_1](data/weights_1.svg)
+
+# Hidden layer 1 activations
+
+![activations_1](data/activations_1.svg)
+
+# Hidden layer 2 weights
+
+![weights_2](data/weights_2.svg)
+
+# Hidden layer 2 activations
+
+![activations_2](data/activations_2.svg)
+
+# Final layer weights
+
+![weights_3](data/weights_3.svg)
--- train/src/validate.rs
+++ train/src/validate.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, fs, path::{Path, PathBuf}, time::Instant};
 use extra::PSFeatures;
-use plotters::{backend::SVGBackend, chart::ChartBuilder, drawing::IntoDrawingArea, series::LineSeries, style::{BLUE, WHITE}};
+use plotters::{backend::SVGBackend, chart::ChartBuilder, drawing::IntoDrawingArea, element::Circle, series::LineSeries, style::{BLUE, Color, WHITE},};
 use serde::Deserialize;
 use tch::{Device, Reduction, Tensor, nn};
 use wazir_drop::{Features, WPSFeatures};
@@ -41,6 +41,8 @@ fn run_with_model<M: EvalModel>(
     let mut model = M::new(features, vs.root(), model_config);
     vs.load(&config.weights)?;
 
+    let mut activation_stats: Vec<ActivationStats> = (0..model.num_layers() - 1).map(|_| ActivationStats::new()).collect();
+
     let mut num_samples = 0;
     let mut total_loss: f64 = 0.0;
     let start_time = Instant::now();
@@ -56,6 +58,11 @@ fn run_with_model<M: EvalModel>(
         );
         num_samples += batch.size;
         total_loss += batch.size as f64 * f64::try_from(&loss).unwrap();
+
+        for (layer, act_stats) in activation_stats.iter_mut().enumerate() {
+            let _guard = tch::no_grad_guard();
+            act_stats.update(model.activations(layer));
+        }
     }
     let elapsed_time = start_time.elapsed().as_secs_f64();
     log::info!(
@@ -65,46 +72,137 @@ fn run_with_model<M: EvalModel>(
         loss = total_loss / num_samples as f64,
     );
 
+    let _guard = tch::no_grad_guard();
     let graph_dir = config_dir.join(&config.graph_dir);
     fs::create_dir_all(&graph_dir)?;
-    plot_weights(&model, &graph_dir)?;
+    for layer in 0..model.num_layers() {
+        let filename = graph_dir.join(format!("weights_{layer}.svg"));
+        let weights = model.layer_weights(layer);
+        plot_weights(&weights, &filename)?;
+    }
+    for (layer, act_stats) in activation_stats.iter().enumerate() {
+        let filename = graph_dir.join(format!("activations_{layer}.svg"));
+        plot_activations(act_stats, &filename)?;
+    }
 
     Ok(())
 }
 
-fn plot_weights<M: EvalModel>(model: &M, graph_dir: &Path) -> Result<(), Box<dyn Error>> {
-    let _guard = tch::no_grad_guard();
+fn plot_weights(weights: &Tensor, filename: &Path) -> Result<(), Box<dyn Error>> {
+    let mut weights: Vec<f64> = weights.try_into().unwrap();
+    weights.sort_by(f64::total_cmp);
 
-    for layer in 0..model.num_layers() {
-        let weights = model.layer_weights(layer);
-        let mut weights: Vec<f64> = weights.try_into().unwrap();
-        weights.sort_by(f64::total_cmp);
+    let root = SVGBackend::new(filename, (1024, 384)).into_drawing_area();
+    root.fill(&WHITE)?;
 
-        let filename = graph_dir.join(format!("weights_{layer}.svg"));
-        let root = SVGBackend::new(&filename, (640, 480)).into_drawing_area();
-        root.fill(&WHITE)?;
+    let mut chart_context = ChartBuilder::on(&root)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("Weight CDF", ("sans-serif", 40))
+        .build_cartesian_2d(*weights.first().unwrap()..*weights.last().unwrap(), 0.0..1.0)?;
 
-        let mut chart_context = ChartBuilder::on(&root)
-            .margin(5)
-            .set_all_label_area_size(50)
-            .build_cartesian_2d(*weights.first().unwrap()..*weights.last().unwrap(), 0.0..1.0)?;
+    chart_context.configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .draw()?;
 
-        chart_context.configure_mesh()
-            .x_labels(20)
-            .y_labels(10)
-            .draw()?;
+    let step_by = (weights.len()/10000).max(1);
+    _ = chart_context.draw_series(LineSeries::new(
+        (0..weights.len()).step_by(step_by).map(
+            |index| (weights[index], index as f64 / (weights.len() - 1) as f64)
+        ),
+        &BLUE
+    ))?;
 
-        let step_by = (weights.len()/10000).max(1);
-        _ = chart_context.draw_series(LineSeries::new(
-            (0..weights.len()).step_by(step_by).map(
-                |index| (weights[index], index as f64 / (weights.len() - 1) as f64)
-            ),
-            &BLUE
-        ))?;
+    root.present()?;
+    log::info!("Written chart {}", filename.display());
+    Ok(())
+}
 
-        root.present()?;
-        log::info!("Written chart {}", filename.display());
+struct ActivationStats {
+    num_samples: i64,
+    mean: Tensor,
+    // sum of (x-mean)^2
+    m2: Tensor,
+}
+
+impl ActivationStats {
+    fn new() -> Self {
+        Self {
+            num_samples: 0,
+            mean: Tensor::new(),
+            m2: Tensor::new(),
+        }
     }
 
+    fn update(&mut self, activations: Tensor) {
+        let n = activations.size()[0];
+        let mean = activations.mean_dim(0, false, tch::Kind::Float);
+        let m2 = activations.var_dim(0, false, false) * (n - 1) as f64;
+
+        if self.num_samples == 0 {
+            self.num_samples = n;
+            self.mean = mean;
+            self.m2 = m2;
+            return;
+        }
+
+        let delta = &mean - &self.mean;
+        let total_samples = self.num_samples + n;
+
+        self.mean += &delta * (n as f64 / total_samples as f64);
+        self.m2 += &m2;
+        self.m2 += &delta.pow_tensor_scalar(2) * (self.num_samples as f64 * n as f64 / total_samples as f64);
+        self.num_samples = total_samples;
+    }
+}
+
+fn plot_activations(act_stats: &ActivationStats, filename: &Path) -> Result<(), Box<dyn Error>> {
+    let means: Vec<f64> = (&act_stats.mean).try_into().unwrap();
+    let stddevs: Vec<f64> = (&act_stats.m2 / (act_stats.num_samples - 1) as f64).sqrt().try_into().unwrap();
+    let mut mean_stddev: Vec<(f64, f64)> = means.into_iter().zip(stddevs).collect();
+    mean_stddev.sort_by(|(m1, _), (m2, _)| m1.total_cmp(m2));
+
+    let root = SVGBackend::new(filename, (1024, 768)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let (upper, lower) = root.split_vertically(768/2);
+
+    let mut chart_context = ChartBuilder::on(&upper)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("Activations", ("sans-serif", 40))
+        .build_cartesian_2d(0.0..1.0, 0.0..0.5)?;
+
+    chart_context.configure_mesh()
+        .x_labels(20)
+        .x_desc("mean")
+        .y_labels(10)
+        .y_desc("stddev")
+        .draw()?;
+
+    _ = chart_context.draw_series(mean_stddev.iter().map(
+        |&(mean, stddev)| Circle::new((mean, stddev), 2, BLUE.filled())
+        ))?;
+
+    let mut chart_context = ChartBuilder::on(&lower)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("Mean CDF", ("sans-serif", 40))
+        .build_cartesian_2d(0.0..1.0, 0.0..1.0)?;
+
+    chart_context.configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .draw()?;
+
+    _ = chart_context.draw_series(LineSeries::new(
+        mean_stddev.iter().enumerate().map(
+            |(index, &(mean, _))| (mean, index as f64 / (mean_stddev.len() - 1) as f64)
+        ),
+        &BLUE
+    ))?;
+
+    root.present()?;
+    log::info!("Written chart {}", filename.display());
     Ok(())
 }
